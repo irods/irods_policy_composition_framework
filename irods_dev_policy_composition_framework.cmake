eb9e6ca94418ecdf0b18e5f838fd59d8ad3d8076
Martin Jaime Flores Jr. <martinflores@renci.org>
2025-09-22 17:35:59 +0000

    [#3] Allow for iRODS 5.X compilation
    Co-authored-by: Markus Kitsinger <root@swooshalicio.us>

--- irods_dev_policy_composition_framework.cmake
+++ irods_dev_policy_composition_framework.cmake
@@ -33,7 +33,6 @@ target_include_directories(
     ${IRODS_EXTERNALS_FULLPATH_JSON}/include
     ${IRODS_EXTERNALS_FULLPATH_JANSSON}/include
     ${IRODS_EXTERNALS_FULLPATH_BOOST}/include
-    ${IRODS_EXTERNALS_FULLPATH_FMT}/include
     ${CMAKE_CURRENT_SOURCE_DIR}/include
     )
 
@@ -46,7 +45,7 @@ target_link_libraries(
     ${IRODS_EXTERNALS_FULLPATH_BOOST}/lib/libboost_thread.so
     ${IRODS_EXTERNALS_FULLPATH_BOOST}/lib/libboost_regex.so
     ${IRODS_EXTERNALS_FULLPATH_BOOST}/lib/libboost_system.so
-    ${IRODS_EXTERNALS_FULLPATH_FMT}/lib/libfmt.so
+    fmt::fmt
     irods_common
     )
 
